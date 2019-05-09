a28f64d75dae05715f82255568e75bc6e4fd43cd
jannesbrunner <jannes.brunner@student.htw-berlin.de>
2019-05-09 13:21:15 +0200

    Project env setup

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-console.log('Hello World!');
+/**
+ * Node interactive course teaching
+ * @author Jannes Brunner
+ * @version 1.0
+ * @copyright 2019
+ */
+
+// Node Imports 
+const http = require('http');
+// 3rd Party Imports
+const express = require('express');
+// App Imports
+
+
+const server = http.createServer( (req, res, next) => {
+    res.write("<h1> Hello World! </h1>");
+    res.end();
+});
+server.listen(3000);
