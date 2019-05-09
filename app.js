684b496a9bf51c2eb30a709541a80903573c0e0c
jannesbrunner <jannes.brunner@student.htw-berlin.de>
2019-05-09 13:50:46 +0200

    Add Error Controller for 404

--- app.js
+++ app.js
@@ -6,11 +6,15 @@
  */
 
 // Node Imports 
-
+const path = require('path');
 // 3rd Party Imports
 const express = require('express');
 // App Imports
+const errorController = require('./controllers/error');
 
 const app = express();
 
 app.listen(3000);
+// Set static content folder
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(errorController.get404);
