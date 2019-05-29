8d0955155a412a5f41cb8c41f9476b6c68baee7b
jannesbrunner <jannes.brunner@student.htw-berlin.de>
2019-05-29 13:38:58 +0200

    Arrow function Settings Model

--- models/settings.js
+++ models/settings.js
@@ -8,8 +8,46 @@
 const db = require('../util/database');
 
 // Model 
-exports.model = class Setting { }
+exports.getSettings = async () => {
+    try {
+        const settings = await db.Settings.findOne({ where: { id: 1 } });
+        if (settings) {
+            return settings.dataValues;
+        } else {
+            throw new Error("DB Get Settings: There are no settings!")
+        }
+    } catch (error) {
+        return error;
+    }
+}
 
+exports.updateSettings = async (updatedSettings) => {
+    try {
+        // check if there are settings present
+        const currentSettings = await db.Settings.findByPk(1);
+
+        // no settings found? Then create them
+        if (!currentSettings) {
+            console.log("No settings found... create new.");
+            const settingsToCreate = await db.Settings.create(updatedSettings)
+            if (settingsToCreate) {
+                return true;
+            } else {
+                throw new Error("DB Create Settings: Error creating settings")
+            }
+        // found settings, update them
+        } else {
+            for (let prop in updatedSettings) {
+                currentSettings[prop] = updatedSettings[prop]
+            }
+            return await currentSettings.save();
+        }
+
+    }
+    catch (error) {
+        return error;
+    }
+}
 
 
 
