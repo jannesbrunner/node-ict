304dfb3cd23e8eb78a9113b4db51cdbd0d99061f
jannesbrunner <jannes.brunner@student.htw-berlin.de>
2019-06-01 19:26:40 +0200

    Get Students for Session

--- models/student.js
+++ models/student.js
@@ -1,3 +1,16 @@
+const db = require('../util/database');
 
+exports.getStudentsForSession = async (sessionId) => {
+    try {
+       const foundStudents = await db.Student.findAndCountAll({where: {eduSessionId: sessionId}});
+       let result = {
+           students: [...foundStudents.rows],
+           amount: foundStudents.count
+       }
+       return result
+    } catch (error) {
+        throw new Error(error)
+    }
+}
 
 
