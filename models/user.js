0989f35344525288c6996cbe8bda52521385a4ce
jannesbrunner <jannes.brunner@student.htw-berlin.de>
2019-06-01 19:27:03 +0200

    Update Error throw + await promise

--- models/user.js
+++ models/user.js
@@ -92,12 +92,12 @@ exports.destroy = async (id) => {
   try {
     const foundUser = await db.User.findByPk(id);
     if(foundUser) {
-    return foundUser.destroy({ force: true });
+    return await foundUser.destroy({ force: true });
     } else {
-      throw new Error(`DB Destroy User: User with ID ${id} does not exist!`);
+      throw new Error(`User with ID ${id} does not exist!`);
     }
   } catch (error) {
-    throw new Error("DB Get User Error " + error)
+    throw new Error("DB Get User Error:  " + error)
   }
 }
 
