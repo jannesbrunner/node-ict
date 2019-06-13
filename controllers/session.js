e50cb2a56cad74c540d4c468e4eaddba2550ce0f
jannesbrunner <jannes.brunner@student.htw-berlin.de>
2019-06-13 19:17:19 +0200

    Start Session logic

--- controllers/session.js
+++ controllers/session.js
@@ -6,11 +6,16 @@
  */
 
 const EduSession = require('../models/eduSession');
+const ip = require('ip');
 
 // GET => /teacher/sessions
 exports.getSessions = async (req, res, next) => {
 
     try {
+        const activeSession = await EduSession.getActiveSession();
+        if (activeSession != null) {
+            return res.redirect('/teacher/sessions/start');
+        }
         let eduSessions;
         if(req.session.user.isSuperAdmin) {
             eduSessions = await EduSession.getAllSessions();
@@ -29,6 +34,29 @@ exports.getSessions = async (req, res, next) => {
         res.render('error', {error: error});
     }
 }
+// GET => /teacher/sessions/start/
+exports.getStartSession = async (req, res, next) => {
+    res.render('teacher/edusessions/running', {
+        docTitle: "Laufende Session | Node ICT",
+        isLoggedIn: req.session.isLoggedIn,
+        loggedUser: req.session.user,
+        ipAdd: ip.address()
+    });
+    
+}
+
+
+// POST => /teacher/sessions/start/:sessionId
+exports.postStartSession = async (req, res, next) => {
+    try {
+        await EduSession.setActiveSession(req.params.sessionId);
+        return res.redirect('/teacher/sessions/start');
+    } catch (error) {
+        res.render('error', {error: error});
+    }
+    
+}
+
 
 
 
