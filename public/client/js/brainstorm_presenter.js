15a2dd94d1118cbc74573ebdcd0d6c9536e15f0b
jannesbrunner <jannes.brunner@student.htw-berlin.de>
2019-06-16 13:21:53 +0200

    Presenter raw setup js

--- public/client/js/brainstorm_presenter.js
+++ public/client/js/brainstorm_presenter.js
@@ -1,3 +1,31 @@
-// eslint-disable-next-line no-undef
-const io = socket;
-console.log("Test!");
+/* eslint-disable no-undef */
+const vue = new Vue({
+    el: '#presenter',
+    data: {
+        sessionName: "",
+        sessionType: "",
+        sessionActive: true,
+        sessionId: "",
+        
+    }, 
+    computed: {  
+        
+    },
+    watch: {
+    },
+    methods: { 
+        sendTest: function() {
+            socket.emit("test", {
+                message: "Hello World!"
+            });
+        }
+    }
+});
+
+
+socket.on('connect', () => {
+   console.log("Connected to server!");
+   socket.emit("attachPresenter", document.getElementById("presenterId").innerHTML);
+});
+
+
