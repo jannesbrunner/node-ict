f7aefed610fe62d5cfbe256bdcadeca9a488539b
jannesbrunner <jannes.brunner@student.htw-berlin.de>
2019-06-19 01:28:19 +0200

    Recatore to new class

--- controllers/games/brainstorming.js
+++ controllers/games/brainstorming.js
@@ -1,27 +1,31 @@
-module.exports = class Brainstorming {
-    constructor(teacherSocket, session) {
-        this.teacherSocket = teacherSocket;
-        this.students = []
-        this.presenters = []
+const logger = require("winston");
+const EduSession = require("../../models/eduSession");
+
+module.exports = class BrainstormTeacher {
+    constructor(connectedStudents, session, socket, teacherId) {
+        this.connectedStudents = connectedStudents;
         this.session = session;
-        this.init();
+        this.socket = socket;
+        this.teacherId = teacherId;
+        this.isRunning = false;
+        this.emitSession();
+       
     }
+    emitSession() {
+        this.socket.emit("session", this.session);
+    }
+    async endSession() {
+        try {
+            const answer = await EduSession.unsetActiveSession(this.teacherId);
+            if (answer) {
+                this.socket.emit("endSession", true);
+            }
+        } catch (error) {
+            this.socket.emit("AppError", { errorMsg: "Error during disabling session!", fatalError: false });
+            logger.log("Error during disabling session! ID: " + this.session.id)
+            // TODO Impement error hadnling
+        }
 
-    init() {
-        console.log("...");
-        this.teacherSocket.emit("session", this.session);
-    }
 
-    addPresenter(psocket) {
-        console.log("New presenter attached!");
-        this.presenters.push(psocket);
-    }
-
-    end() {
-        this.teacher.emit("endSession", true);   
-    }
-    save() {
-        console.log("Saving game...");
     }
 }
-
