cabb3b296f890fb8d73bb2ae346c3e271755dadd
jannesbrunner <jannes.brunner@student.htw-berlin.de>
2019-06-19 01:31:08 +0200

    Add winston logger

--- util/db_setup.js
+++ util/db_setup.js
@@ -1,23 +1,27 @@
 const db = require('./database');
+const logger = require('winston');
+
 
 exports.connect = async () => {
   try {
     db.sequelize.authenticate();
-    console.log('Database Connection has been established successfully.');
+    logger.log('debug', 'Database Connection has been established successfully.');
     return true;
   } catch (err) {
-    console.error('Unable to connect to the database:', err);
+    logger.log('error', `Unable to connect to the database: ${err}`);
     return err;
   }
 }
 
 async function syncDB( options = {} ) {
   try {
-    console.log(`Sync Databse with Option(s): ${JSON.stringify(options)}`)
+    
+    logger.log('debug', `Sync Databse with Option(s): ${JSON.stringify(options)}`);
     db.sequelize.sync(options);
     // await db.User.sync();  
     // await db.Settings.sync();
   } catch (error) {
+    logger.log('error', `DB Sync Error: ${error}`);
     throw new Error(`DB Sync Error: ${error}`);
   }
 }
---
+++ util/loggerSetup.js
@@ -0,0 +1,39 @@
+
+const { format, createLogger, transports } = require('winston');
+const { combine, timestamp, printf } = format;
+
+const logger = require('winston');
+
+const myFormat = printf(({ level, message, timestamp }) => {
+    return `${timestamp} [${level}]: ${message}`;
+});
+
+logger.configure({
+    level: 'info',
+    format: combine(
+        format.timestamp({
+            format: 'YYYY-MM-DD HH:mm:ss'
+        }),
+        myFormat
+    ),
+    transports: [
+        //
+        // - Write to all logs with level `info` and below to `combined.log` 
+        // - Write all logs error (and below) to `error.log`.
+        //
+        new transports.File({ filename: 'error.log', level: 'error' }),
+        new transports.File({ filename: 'combined.log' }),
+        //
+        // If we're not in production then log to the `console` with the format:
+        // `${info.level}: ${info.message} JSON.stringify({ ...rest }) `
+        // 
+        process.env.NODE_ENV !== 'production' ? new transports.Console({ format: myFormat }) : null,
+
+    ]
+});
+
+module.exports = logger;
+
+
+
+
