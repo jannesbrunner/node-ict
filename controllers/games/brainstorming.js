09bf10fcc1497c89dae0b8f1c688c3a72ccb7534
jannesbrunner <jannes.brunner@student.htw-berlin.de>
2019-06-19 22:10:09 +0200

    Refactore + Add Player function

--- controllers/games/brainstorming.js
+++ controllers/games/brainstorming.js
@@ -1,13 +1,18 @@
 const logger = require("winston");
 const EduSession = require("../../models/eduSession");
+const Student = require("../../models/student");
+
 
 module.exports = class BrainstormTeacher {
-    constructor(connectedStudents, session, socket, teacherId) {
-        this.connectedStudents = new Set()
+    constructor(session, socket) {
         this.session = session;
         this.socket = socket;
-        this.teacherId = teacherId;
-        this.isRunning = false;
+        
+        this.teacherId = session.userId;
+        this.isRunning = session.isRunning;
+        
+        this.studentSockets = [];
+
         this.emitSession();
         this.emitPlayerList();
     }
@@ -15,21 +20,53 @@ module.exports = class BrainstormTeacher {
         this.socket.emit("session", this.session);
     }
     emitPlayerList() {
-        const playerList = [];
-        this.connectedStudents.forEach(  
-            (player) => {
-                playerList.push({name: player.name});
-            })
-            this.socket.emit("updatePlayerList", playerList);
+        Student.getStudentsForSession(this.session.id).then(
+            (players) => {
+                if(players) {
+                    this.socket.emit("updatePlayerList", players.students);
+                }
+            }
+        ).catch(
+            (error) => {
+                this.socket.emit("appError", 
+                {errorMsg:`Unable to emit playerList (id ${this.session.id}): ${error}`, fatalError: false});
+            }
+        )
     }
     // TODO ALLES MIT INS SESSION OBJEKT?
     addPlayer(player) {
+        
         logger.log("debug", `Add Player NAME ${player.name}, SOCKET: ${player.socket} 
         to game ${this.session.id}, ${this.session.name}`);
-        this.connectedStudents.add({name: player.name, socket: player.socket});
-        this.emitPlayerList();
-        player.socket.emit("gameJoined", {type: "brainstorming", teacherId: this.teacherId});
+
+        Student.addStudentToSession(this.session.id, player.name).then( (
+            (result) => {
+                if(result) {
+                    this.studentSockets.push(player.socket);
+                    player.socket.emit("gameJoined", {type: "brainstorming", teacherId: this.teacherId});
+                    this.studentSockets.push(player.socket);
+                    this.emitPlayerList();
+                }
+            }
+        )).catch(
+            (error) => {
+                player.socket.emit("appError", 
+                {errorMsg:`Unable to join game (id ${this.session.id}) (owner ${this.teacherId}): ${error}`, fatalError: false});
+                logger.log("warn", `Unable to join game (id ${this.session.id}) (owner ${this.teacherId}): ${error}`);
+            }
+        )
+        
+        
+        
     }
+
+    removePlayer(player) {
+        logger.log("debug", `Remove Player NAME ${player.name}, SOCKET: ${player.socket} 
+        from game ${this.session.id}, ${this.session.name}`);
+
+      
+    }
+
     async endSession() {
         try {
             const answer = await EduSession.unsetActiveSession(this.teacherId);
