a13cfbbf90089a4833cf3d32200088a5323211d9
jannesbrunner <jannes.brunner@student.htw-berlin.de>
2019-06-25 21:06:07 +0200

    Refactor code layout

--- public/client/js/presenter/dev.js
+++ public/client/js/presenter/dev.js
@@ -1,87 +1,125 @@
-var cloud = require("vue-d3-cloud");
-
+//var cloud = require("vue-d3-cloud");
+const Vue = require('vue');
+const zingchart = require('zingchart');
 /* eslint-disable no-undef */
+
+// Vue.component('vue-word-cloud', VueWordCloud);
 
 
 const vue = new Vue({
     el: '#presenter',
     data: {
-        sessionId: "",
-        type: "",
-        teacherId: null,
-        isRunning: false,
-        words: [['Das', 19], ['Ist', 3], ['ein', 7], ['Test', 3]],
-        enterAnimation: {
-            opacity: 0,
-            transform: 'scale3d(0.3,0.3,0.3)'},
-        
-        
+        session: null,
+        renderBS: false,
+        renderQZ: false,
+        cloudWords: [],
     },
-   
-    mount() {
-        
-    }, 
-    computed: {  
-        
+    mounted() {
+        this.socketIO();
+        // Clean exit on closed window
+        window.addEventListener('unload', function (event) {
+            event.preventDefault();
+            socket.emit("presenterLeft", true);
+        })
+    },
+    computed: {
+        cloudText: function () {
+            return this.cloudWords.join(" ");
+        }
     },
     watch: {
     },
-    methods: { 
-        sendTest: function() {
+    methods: {
+        sendTest: function () {
             socket.emit("test", {
                 message: "Hello World!"
             });
         },
-        onWordClick: function(word) {
-            alert(word);
+        renderCloud: function () {
+            zingchart.render({
+                id: 'cloud',
+                data: {
+                    type: 'wordcloud',
+                    options: {
+                        text: vue.cloudText,
+                    }
+                },
+                height: 400,
+                width: '100%'
+
+            });
+        },
+        socketIO: function () {
+            socket.on('connect', () => {
+                console.log("Connected to server!");
+                vue.sessionId = document.getElementById("presenterId").innerHTML;
+                console.log(`Attach myself as Presenter for Session with id ${vue.sessionId}`)
+                socket.emit("attachPresenter", vue.sessionId);
+                window.addEventListener('beforeunload', function (e) {
+                    //Cancel the event
+                    e.preventDefault();
+                    // Chrome requires returnValue to be set
+                    e.returnValue = 'HALLO';
+
+                });
+            });
+
+            socket.on("game", function (data) {
+                console.log(data);
+                vue.session = data.session;
+                switch (vue.session.type) {
+                    case "brainstorming":
+                        vue.renderBS = true;
+                        break;
+                    case "quizzing":
+                        
+                        break;
+                
+                    default:
+                        break;
+                }
+            });
+
+            socket.on('disconnect', () => {
+                Swal.fire({
+                    title: 'Error!',
+                    text: 'Verbindung verloren!',
+                    type: 'error',
+                    confirmButtonText: 'OK'
+                })
+            });
+
+            socket.on('appError', function (error) {
+
+                Swal.fire({
+                    type: 'error',
+                    title: 'Oops...',
+                    text: error.errorMsg,
+                    footer: 'Bitte an den Support wenden!'
+                })
+
+                if (error.fatalError) {
+                    //  vue.sessionActive = false;
+                }
+
+            })
         }
-    }
+    },
+    filters: {
+        capitalize: function (value) {
+          if (!value) return ''
+          value = value.toString()
+          return value.charAt(0).toUpperCase() + value.slice(1)
+        }
+      }
 });
 
 
 
 
-// Listen for Events
-socket.on("game", function(data) {
-    console.log(data);
-    vue.sessionId = data.id;
-    vue.type = data.type;
-    vue.teacherId = data.teacherId;
-    vue.isRunning = data.isRunning; 
-});
 
-socket.on('disconnect', () => {
-    Swal.fire({
-        title: 'Error!',
-        text: 'Verbindung verloren!',
-        type: 'error',
-        confirmButtonText: 'OK'
-      })
- });
 
-// Emit Events
-window.addEventListener('unload', function(event) {
-    event.preventDefault();
-    socket.emit("presenterLeft", true);
-})
 
-socket.on('connect', () => {
-
-    console.log("Connected to server!");
-    vue.sessionId = document.getElementById("presenterId").innerHTML;
-    console.log(`Attach myself as Presenter for Session with id ${vue.sessionId}`)
-    socket.emit("attachPresenter", vue.sessionId);
-    window.addEventListener('beforeunload', function (e) {
-        //Cancel the event
-        e.preventDefault();
-        // Chrome requires returnValue to be set
-        e.returnValue = 'HALLO';
-        
-      });
- });
-
- alert("hallo");
- 
 
 
 
